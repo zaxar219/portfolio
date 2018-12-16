206c6c4c68757215e36ba55089e4a5d15c86e5af
Vladimir.Zaharov <zaxar219@mail.ru>
2018-12-16 19:45:44 +0300

    dz-hom-1

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,61 @@
-$(document).ready(function() {
-
+$(document).ready(function(){
 	
-	//Плавная прокрутка
-	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-	    highlightSelector:"nav a"
+	// Скрипт для показа и скрытия выпадающего меню на смартфонах
+	//Создали переменные
+	var pull = $("#navigation-toggle");
+	var menu = $(".navigation ul");
+	
+	
+	//Описание событие при нажатии на ссылку
+	$(pull).on("click", function(e){
+		//Отменяем стандартное повидение ссылки в браузере
+		e.preventDefault();
+		//Открываем/Скрываем меню
+
+		menu.slideToggle();
+		
+		//Добовляем модификатор --activ
+		$(this).toggleClass('navigation__toggle-button--active');
 	});
 
+	
+	$(window).resize(function(){
+	    var w = $(window).width();
+	    if(w > 768) {
+	        menu.removeAttr('style');
+	        pull.removeClass('navigation__toggle-button--active');
+	    } else {
 
-}); 
+	    }
+	});
+
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 768 && nav.is(':hidden')) {
+			nav.removeAttr('style');
+		}
+	});
+	
+	// Вызов слайдера  
+	$("#top-slider").owlCarousel({
+		singleItem: true,
+	});
+	
+	$("#agents-slider").owlCarousel({
+		singleItem: true,
+		
+	});
+	
+	$("#bottom-slider").owlCarousel({
+		singleItem: true,
+		
+		navigation: true,
+		navigationText: ["",""],
+		slideSpeed: 500
+	});
+	
+	
+	
+	
+	
+});
